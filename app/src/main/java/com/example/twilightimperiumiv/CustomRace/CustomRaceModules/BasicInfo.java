d9695edccab27013c507490acca276788e4eae4a
ashlee.williamson <ashleecw@gmail.com>
2019-09-10 22:37:26 +0100

    Fleshed out BasicInfo for creating a Race module

--- app/src/main/java/com/example/twilightimperiumiv/CustomRace/CustomRaceModules/BasicInfo.java
+++ app/src/main/java/com/example/twilightimperiumiv/CustomRace/CustomRaceModules/BasicInfo.java
@@ -1,4 +1,50 @@
 package com.example.twilightimperiumiv.CustomRace.CustomRaceModules;
 
 public class BasicInfo {
+
+    String name;
+    String planetName;
+    int planetResources;
+    int planetInfluence;
+    String lore;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlanetName() {
+        return planetName;
+    }
+
+    public void setPlanetName(String planetName) {
+        this.planetName = planetName;
+    }
+
+    public int getPlanetResources() {
+        return planetResources;
+    }
+
+    public void setPlanetResources(int planetResources) {
+        this.planetResources = planetResources;
+    }
+
+    public int getPlanetInfluence() {
+        return planetInfluence;
+    }
+
+    public void setPlanetInfluence(int planetInfluence) {
+        this.planetInfluence = planetInfluence;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
 }
