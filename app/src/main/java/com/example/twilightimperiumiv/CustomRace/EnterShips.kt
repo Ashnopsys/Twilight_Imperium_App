7243249a587de963affec9e195735c6a1b32c139
ashlee.williamson <ashleecw@gmail.com>
2019-10-16 16:42:13 +0100

    Added the add ships method

--- app/src/main/java/com/example/twilightimperiumiv/CustomRace/EnterShips.kt
+++ app/src/main/java/com/example/twilightimperiumiv/CustomRace/EnterShips.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.app.AppCompatActivity
+import com.example.twilightimperiumiv.CustomRace.Ships.Ship
+import com.example.twilightimperiumiv.CustomRace.Ships.ShipType
 import com.example.twilightimperiumiv.R
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_one.*
@@ -14,5 +16,23 @@ override fun onCreate(savedInstanceState: Bundle?) {
     setContentView(R.layout.fragment_one)
     val customRace = CustomRace()
 
+
+    val from = intent.extras.getString("CUSTOM_RACE")
+
+
 }
+    fun createFleet(): ArrayList<Ship> {
+        /*
+        Create all the ships with default stats. Will create another method to change stats
+        based on selected ship
+         */
+        var DEFAULT_CUSTOM_STATS = false
+        var fleet: ArrayList<Ship> = ArrayList()
+
+        for (ship in ShipType.values()) {
+            var newShip = Ship(ship, DEFAULT_CUSTOM_STATS , fleet)
+            fleet.add(newShip)
+        }
+        return fleet
+    }
 }
