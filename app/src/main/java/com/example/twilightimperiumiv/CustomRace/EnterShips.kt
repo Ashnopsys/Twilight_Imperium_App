437714ea6d6015a1a5862dd643d1ad2026e02a4d
ashlee.williamson <ashleecw@gmail.com>
2019-11-08 21:47:15 +0000

    All ship dropdowns working, need to correct default ship stats

--- app/src/main/java/com/example/twilightimperiumiv/CustomRace/EnterShips.kt
+++ app/src/main/java/com/example/twilightimperiumiv/CustomRace/EnterShips.kt
@@ -4,48 +4,69 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
 import com.example.twilightimperiumiv.CustomRace.Ships.Ship
 import com.example.twilightimperiumiv.CustomRace.Ships.ShipType
 import kotlinx.android.synthetic.main.fragment_three.*
 import com.example.twilightimperiumiv.Fragments.FX
+import com.example.twilightimperiumiv.R
 import kotlinx.android.synthetic.main.create_destroyer.view.*
 
 
-class EnterShips  : AppCompatActivity() {
-    lateinit var carrier_dropdown : View
-    lateinit var cruiser_dropdown : View
-    lateinit var destroyer_dropdown : View
-    lateinit var dreadnought_dropdown : View
-    lateinit var fighter_dropdown : View
-    lateinit var infantry_dropdown : View
-    lateinit var pds_dropdown : View
-    lateinit var warsun_dropdown : View
+class EnterShips : AppCompatActivity() {
+
+    lateinit var carrier_dropdown: View
+    lateinit var cruiser_dropdown: View
+    lateinit var destroyer_dropdown: View
+    lateinit var dreadnought_dropdown: View
+    lateinit var fighter_dropdown: View
+    lateinit var infantry_dropdown: View
+    lateinit var pds_dropdown: View
+    lateinit var warsun_dropdown: View
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(com.example.twilightimperiumiv.R.layout.fragment_three)
-    val customRace = CustomRace()
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.twilightimperiumiv.R.layout.fragment_three)
+        val customRace = CustomRace()
 
-    val inflater: LayoutInflater = LayoutInflater.from(applicationContext)
-    carrier_dropdown = inflater.inflate(com.example.twilightimperiumiv.R.layout.create_carrier, carrier_layout, false)
-    carrier_layout.addView(carrier_dropdown)
+        val inflater: LayoutInflater = LayoutInflater.from(applicationContext)
 
-    cruiser_dropdown = inflater.inflate(com.example.twilightimperiumiv.R.layout.create_cruiser, cruiser_layout, false)
-    cruiser_layout.addView(cruiser_dropdown)
-    destroyer_dropdown = inflater.inflate(com.example.twilightimperiumiv.R.layout.create_cruiser, destroyer_layout, false)
-            destroyer_layout.addView(destroyer_dropdown)
-    val ship_views = arrayOf(carrier_dropdown, cruiser_dropdown)
+        carrier_dropdown = inflater.inflate(R.layout.create_carrier, carrier_layout, false)
+        cruiser_dropdown = inflater.inflate(R.layout.create_cruiser, cruiser_layout, false)
+        destroyer_dropdown = inflater.inflate(R.layout.create_destroyer, destroyer_layout, false)
+        dreadnought_dropdown = inflater.inflate(R.layout.create_dreadnought, dreadnought_layout, false)
+        fighter_dropdown = inflater.inflate(R.layout.create_fighter, fighter_layout, false)
+        infantry_dropdown = inflater.inflate(R.layout.create_infantry, infantry_layout, false)
+        pds_dropdown = inflater.inflate(R.layout.create_pds, pds_layout, false)
+        warsun_dropdown = inflater.inflate(R.layout.create_warsun, warsun_layout, false)
 
+        val ship_views = arrayOf(carrier_dropdown, cruiser_dropdown, destroyer_dropdown, dreadnought_dropdown,
+                fighter_dropdown, infantry_dropdown, pds_dropdown, warsun_dropdown)
+        val ship_xmls = arrayOf(R.layout.create_carrier, R.layout.create_cruiser, R.layout.create_destroyer,
+                R.layout.create_dreadnought, R.layout.create_fighter, R.layout.create_infantry,
+                R.layout.create_pds, R.layout.create_warsun)
+        val ship_layouts = arrayOf(carrier_layout, cruiser_layout, destroyer_layout, dreadnought_layout,
+                fighter_layout, infantry_layout, pds_layout, warsun_layout)
+
+        addViews(ship_views, ship_layouts)
 //            , destroyer_dropdown, dreadnought_dropdown,
 //                infantry_dropdown, pds_dropdown, warsun_dropdown)
 
 
         makeAllViewsGone(ship_views)
 
-    val from = intent.extras.getString("CUSTOM_RACE")
+        val from = intent.extras.getString("CUSTOM_RACE")
 
-    //TODO need to figure out how to apply clicked method to each of the buttons without spaghetti code
-}
+        //TODO need to figure out how to apply clicked method to each of the buttons without spaghetti code
+    }
+
+    private fun addViews(ship_views: Array<View>, ship_layouts: Array<LinearLayout>) {
+        for (i in ship_views.indices) {
+            ship_layouts[i].addView(ship_views[i])
+        }
+    }
+
     fun createFleet(): ArrayList<Ship> {
         /*
         Create all the ships with default stats. Will create another method to change stats
@@ -55,7 +76,7 @@ class EnterShips  : AppCompatActivity() {
         var fleet: ArrayList<Ship> = ArrayList()
 
         for (ship in ShipType.values()) {
-            var newShip = Ship(ship, DEFAULT_CUSTOM_STATS , fleet)
+            var newShip = Ship(ship, DEFAULT_CUSTOM_STATS, fleet)
             fleet.add(newShip)
         }
         return fleet
@@ -79,7 +100,7 @@ class EnterShips  : AppCompatActivity() {
         }
     }
 
-    fun toggle_contents_cruiser(v : View) {
+    fun toggle_contents_cruiser(v: View) {
         if (cruiser_dropdown.isShown) {
             FX.slide_up(this, cruiser_dropdown)
             //TODO fix slide up
@@ -89,7 +110,8 @@ class EnterShips  : AppCompatActivity() {
             FX.slide_down(this, cruiser_dropdown)
         }
     }
-    fun toggle_contents_destroyer(v : View) {
+
+    fun toggle_contents_destroyer(v: View) {
         if (destroyer_dropdown.isShown) {
             FX.slide_up(this, destroyer_dropdown)
             //TODO fix slide up
@@ -100,7 +122,7 @@ class EnterShips  : AppCompatActivity() {
         }
     }
 
-    fun toggle_contents_dreadnought(v : View) {
+    fun toggle_contents_dreadnought(v: View) {
         if (dreadnought_dropdown.isShown) {
             FX.slide_up(this, dreadnought_dropdown)
             //TODO fix slide up
@@ -110,7 +132,8 @@ class EnterShips  : AppCompatActivity() {
             FX.slide_down(this, dreadnought_dropdown)
         }
     }
-    fun toggle_contents_fighter(v : View) {
+
+    fun toggle_contents_fighter(v: View) {
         if (fighter_dropdown.isShown) {
             FX.slide_up(this, fighter_dropdown)
             //TODO fix slide up
@@ -120,7 +143,8 @@ class EnterShips  : AppCompatActivity() {
             FX.slide_down(this, fighter_dropdown)
         }
     }
-    fun toggle_contents_infantry(v : View) {
+
+    fun toggle_contents_infantry(v: View) {
         if (infantry_dropdown.isShown) {
             FX.slide_up(this, infantry_dropdown)
             //TODO fix slide up
@@ -130,7 +154,8 @@ class EnterShips  : AppCompatActivity() {
             FX.slide_down(this, infantry_dropdown)
         }
     }
-    fun toggle_contents_pds(v : View) {
+
+    fun toggle_contents_pds(v: View) {
         if (pds_dropdown.isShown) {
             FX.slide_up(this, pds_dropdown)
             //TODO fix slide up
@@ -140,7 +165,8 @@ class EnterShips  : AppCompatActivity() {
             FX.slide_down(this, pds_dropdown)
         }
     }
-    fun toggle_contents_warsun(v : View) {
+
+    fun toggle_contents_warsun(v: View) {
         if (warsun_dropdown.isShown) {
             FX.slide_up(this, warsun_dropdown)
             //TODO fix slide up
